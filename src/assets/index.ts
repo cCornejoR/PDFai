1126dda157a1d3976eaee54235c3bcab98efef54
Crhistian Cornejo <94501911+cCornejoR@users.noreply.github.com>
2025-07-01 11:07:51 -0500

    fix(asset-import): correct Logo.svg path

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,6 @@
 // Assets exportados para uso en componentes React
 import bckImage from "./bck.png";
-import logoSvg from "./logo.svg";
+import logoSvg from "./Logo.svg";
 
 export { bckImage, logoSvg };
 
