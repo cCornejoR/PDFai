fa6b07f66b4648cd07ff09ccff9483cb59ae9251
Crhistian Cornejo <94501911+cCornejoR@users.noreply.github.com>
2025-07-01 13:33:49 -0500

    chore: consolidate ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,18 @@ import js from '@eslint/js';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import importPlugin from 'eslint-plugin-import';
+import globals from 'globals';
 
 export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
+  },
   js.configs.recommended,
   {
     files: ['**/*.ts', '**/*.tsx'],
@@ -21,8 +31,10 @@ export default [
       import: importPlugin,
     },
     rules: {
+      ...(tseslint.configs.eslintRecommended?.overrides?.[0]?.rules || {}),
       ...tseslint.configs.recommended.rules,
       ...importPlugin.configs.recommended.rules,
+      ...(importPlugin.configs.electron?.rules || {}),
       ...(importPlugin.configs.typescript?.rules || {}),
     },
   },
